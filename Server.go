cc0e3929bdc22ab1be4f94daae1aac74b112de61
DarthBaluu <axel.lammertz@gmx.de>
2019-05-06 18:01:18 +0200

    Abgabe4/5 fertig

---
+++ Server.go
@@ -0,0 +1,41 @@
+package main
+
+import(
+	"net/http"
+	"./controller"
+)
+
+func main(){
+	
+	http.HandleFunc("/eingeloggt_startseite", controller.Eingeloggt_start)
+	http.HandleFunc("/eingeloggt_karteikasten", controller.Eingeloggt_karteikasten)
+	http.HandleFunc("/eingeloggt_meinekarteien", controller.Eingeloggt_meineKarteien)
+	http.HandleFunc("/eingeloggt_meinprofil", controller.Eingeloggt_meinProfil)
+	http.HandleFunc("/eingeloggt_karteierstellen_01", controller.Eingeloggt_karteiErstellen_01)
+	http.HandleFunc("/eingeloggt_karteierstellen_02", controller.Eingeloggt_karteiErstellen_02)
+	http.HandleFunc("/eingeloggt_karteikastenansehen", controller.Eingeloggt_karteikastenAnsehen)
+	http.HandleFunc("/eingeloggt_lernen_01", controller.Eingeloggt_lernen_01)
+	http.HandleFunc("/eingeloggt_lernen_02", controller.Eingeloggt_lernen_02)
+	http.HandleFunc("/eingeloggt_profilloeschen", controller.Eingeloggt_profilLoeschen)
+
+	
+	http.HandleFunc("/ausgeloggt_startseite", controller.Ausgeloggt_start)
+	http.HandleFunc("/ausgeloggt_karteikasten", controller.Ausgeloggt_karteikasten)
+	http.HandleFunc("/ausgeloggt_registrieren", controller.Ausgeloggt_registrieren)
+
+	//bereitstellung der statischen inhalte
+	http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("./static/css"))))
+	http.Handle("/favicons/", http.StripPrefix("/favicons/", http.FileServer(http.Dir("./static/favicons"))))
+	http.Handle("/font/", http.StripPrefix("/font/", http.FileServer(http.Dir("./static/font"))))
+	http.Handle("/icons/", http.StripPrefix("/icons/", http.FileServer(http.Dir("./static/icons"))))
+	http.Handle("/js/", http.StripPrefix("/js/", http.FileServer(http.Dir("./static/js"))))
+	http.Handle("/logo/", http.StripPrefix("/logo/", http.FileServer(http.Dir("./static/logo"))))
+
+	server := http.Server{
+		Addr: ":8080",
+	}
+
+	server.ListenAndServe()
+
+}
+
--- controller/controller.go
+++ controller/controller.go
@@ -1,73 +1,73 @@
 package controller
 
-import{
+import(
 	"net/http"
-	"html/templates"
-}
+	"html/template"
+)
 
 /* ######################   not logged in Pages   ###################### */
-func ausgeloggt_start(w http.ResponseWriter, r *http.Request) {
-	t, _ := template.ParseFiles("../Templates/ausgeloggt_main.html", "./Templates/beides_startseite.html")
+func Ausgeloggt_start(w http.ResponseWriter, r *http.Request) {
+	t, _ := template.ParseFiles("./Templates/ausgeloggt_main.html", "./Templates/beides_startseite.html")
 	t.ExecuteTemplate(w, "layout", "")
 }
 
-func ausgeloggt_karteikasten(w http.ResponseWriter, r *http.Request) {
+func Ausgeloggt_karteikasten(w http.ResponseWriter, r *http.Request) {
 	t, _ := template.ParseFiles("./Templates/ausgeloggt_main.html", "./Templates/ausgeloggt_karteikasten.html")
 	t.ExecuteTemplate(w, "layout", "")
 }
 
-func ausgeloggt_registrieren(w http.ResponseWriter, r *http.Request) {
+func Ausgeloggt_registrieren(w http.ResponseWriter, r *http.Request) {
 	t, _ := template.ParseFiles("./Templates/ausgeloggt_main.html", "./Templates/ausgeloggt_registrieren.html")
 	t.ExecuteTemplate(w, "layout", "")
 }
 
 /* ######################   logged in Pages   ###################### */
-func eingeloggt_start(w http.ResponseWriter, r *http.Request) {
+func Eingeloggt_start(w http.ResponseWriter, r *http.Request) {
 	t, _ := template.ParseFiles("./Templates/eingeloggt_main.html", "./Templates/beides_startseite.html")
 	t.ExecuteTemplate(w, "layout", "")
 }
 
-func eingeloggt_karteikasten(w http.ResponseWriter, r *http.Request) {
+func Eingeloggt_karteikasten(w http.ResponseWriter, r *http.Request) {
 	t, _ := template.ParseFiles("./Templates/eingeloggt_main.html", "./Templates/eingeloggt_karteikasten.html")
 	t.ExecuteTemplate(w, "layout", "")
 }
 
-func eingeloggt_meineKarteien(w http.ResponseWriter, r *http.Request) {
+func Eingeloggt_meineKarteien(w http.ResponseWriter, r *http.Request) {
 	t, _ := template.ParseFiles("./Templates/eingeloggt_main.html", "./Templates/eingeloggt_meinekarteien.html")
 	t.ExecuteTemplate(w, "layout", "")
 }
 
-func eingeloggt_meinProfil(w http.ResponseWriter, r *http.Request) {
+func Eingeloggt_meinProfil(w http.ResponseWriter, r *http.Request) {
 	t, _ := template.ParseFiles("./Templates/eingeloggt_main.html", "./Templates/eingeloggt_meinprofil.html")
 	t.ExecuteTemplate(w, "layout", "")
 }
 
-func eingeloggt_karteiErstellen_01(w http.ResponseWriter, r *http.Request) {
+func Eingeloggt_karteiErstellen_01(w http.ResponseWriter, r *http.Request) {
 	t, _ := template.ParseFiles("./Templates/eingeloggt_main.html", "./Templates/eingeloggt_karteierstellen_01.html")
 	t.ExecuteTemplate(w, "layout", "")
 }
 
-func eingeloggt_karteiErstellen_02(w http.ResponseWriter, r *http.Request) {
+func Eingeloggt_karteiErstellen_02(w http.ResponseWriter, r *http.Request) {
 	t, _ := template.ParseFiles("./Templates/eingeloggt_main.html", "./Templates/eingeloggt_karteierstellen_02.html")
 	t.ExecuteTemplate(w, "layout", "")
 }
 
-func eingeloggt_karteikastenAnsehen(w http.ResponseWriter, r *http.Request) {
+func Eingeloggt_karteikastenAnsehen(w http.ResponseWriter, r *http.Request) {
 	t, _ := template.ParseFiles("./Templates/eingeloggt_main.html", "./Templates/eingeloggt_karteikastenansehem.html")
 	t.ExecuteTemplate(w, "layout", "")
 }
 
-func eingeloggt_lernen_01(w http.ResponseWriter, r *http.Request) {
+func Eingeloggt_lernen_01(w http.ResponseWriter, r *http.Request) {
 	t, _ := template.ParseFiles("./Templates/eingeloggt_main.html", "./Templates/eingeloggt_lernen_01.html")
 	t.ExecuteTemplate(w, "layout", "")
 }
 
-func eingeloggt_lernen_02(w http.ResponseWriter, r *http.Request) {
+func Eingeloggt_lernen_02(w http.ResponseWriter, r *http.Request) {
 	t, _ := template.ParseFiles("./Templates/eingeloggt_main.html", "./Templates/eingeloggt_lernen_02.html")
 	t.ExecuteTemplate(w, "layout", "")
 }
 
-func eingeloggt_profilLoeschen(w http.ResponseWriter, r *http.Request) {
+func Eingeloggt_profilLoeschen(w http.ResponseWriter, r *http.Request) {
 	t, _ := template.ParseFiles("./Templates/eingeloggt_main.html", "./Templates/eingeloggtprofilloeschen.html")
 	t.ExecuteTemplate(w, "layout", "")
 }
